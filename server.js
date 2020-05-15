7b83bb5aaeb49d1f5b6b0603da9372a2ccc36c2b
Wasurat <Aumaumone2009@gmail.com>
2020-05-15 23:30:12 +0700

    final

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const path = require('path');
+const transactions = require('./routes/transactions');
+const app = express()
+const connectDB = require('./config/db');
+
+dotenv.config({ path : './config/config.env' })
+
+connectDB()
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.use(express.json())
+// app.use(express.bodyParser())
+app.use('/api/v1/transactions' , transactions)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*' , (req , res) => res.sendFile(path.resolve(__dirname,'client','build','index')))
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT , console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`.rainbow.underline))
+
